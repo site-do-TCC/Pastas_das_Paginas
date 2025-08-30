166e209d4622efbdc2ad37bc7bcd6d340415c226
nathanael lima rodrigues <nathanaellima572.com>
2025-08-30 18:32:22 -0300

    terminei

--- js/login.js
+++ js/login.js
@@ -15,4 +15,21 @@ menuBtn.addEventListener("click", () => {
     toggleBtn.addEventListener("click", () => {
       const type = senhaInput.getAttribute("type") === "password" ? "text" : "password";
       senhaInput.setAttribute("type", type);
-    });
+
+    }
+  
+  
+  );
+
+  const alerta = document.getElementById("alertaErro");
+  const fecharAlerta = document.getElementById("fecharAlerta");
+
+  // codigo da tela nao sei se ta certo nao ta dando certo
+  function mostrarErro() {
+    alerta.style.display = "flex";
+  }
+
+  fecharAlerta.addEventListener("click", () => {
+    alerta.style.display = "none";
+  });
+  
