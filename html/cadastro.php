bdfba899b7897567adcd4e67746a598b2d0c10fd
Murilo Alves da Fonseca <muriloalves.fonseca08@gmail.com>
2025-09-06 21:27:10 -0300

    Tenho que mudar os links das imagens e do css no login. E só o das imagens no cadastro

--- html/cadastro.php
+++ html/cadastro.php
@@ -1,10 +1,11 @@
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cadastro - Avena</title>
-  <link rel="stylesheet" href="/css/cadastro.css">
+  <link rel="stylesheet" href="\Programacao_TCC_Avena\css\cadastro.css">
 </head>
 <body>
   <header>
