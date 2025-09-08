5dad2c782843aab345103210ef5c6bfffb688e04
Murilo Alves da Fonseca <muriloalves.fonseca08@gmail.com>
2025-09-08 13:48:26 -0300

    Sistema de CADASTRO completo com sucesso, por enquando ta funcionando, ele ta cadastrando nas 2 tabelas dependendo do tipo de usuario selecionado no select

--- html/cadastro.php
+++ html/cadastro.php
@@ -1,3 +1,36 @@
+<?php
+  if(isset($_POST['submit'])){
+    print_r('Nome: ' . $_POST['nome']);
+    print_r('<br>');
+    print_r('Email: ' . $_POST['email']);
+    print_r('<br>');
+    print_r('Senha: ' . $_POST['senha']);
+    print_r('<br>');
+    print_r('Tipo: ' . $_POST['tipo']);
+    print_r('<br>');
+    print_r('<hr>');
+
+    //if ($_POST['tipo'] == 'profissional'){
+    //  print_r('É profissional');
+    //}else{
+    //  print_r('Não é profissional, é contratante!');
+    //}
+
+    include_once('C:\usbwebserver_v8.6.5\usbwebserver\root\Programacao_TCC_Avena\php\conexao.php');
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    print_r('Teste conexão: ' . $dbUsername);
+
+    if ($_POST['tipo'] == 'profissional'){
+      $result = mysqli_query($conexao, "INSERT INTO prestadora(nome,email,senha) VALUES ('$nome','$email','$senha')");
+    }else{
+      $result = mysqli_query($conexao, "INSERT INTO cliente(nome,email,senha) VALUES ('$nome','$email','$senha')");
+    }
+  }
+?>
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -16,7 +49,7 @@
 
   <main class="container">
     <div class="form-section">
-      <form>
+      <form action="cadastro.php" method="POST">
         <label for="nome">Nome</label>
         <input type="text" id="nome" name="nome" required>
 
@@ -38,7 +71,7 @@
           <label for="termos">Termos de Privacidade</label>
         </div>
 
-        <button id="btnSubmit" type="submit" class="btn-cadastrar" onclick="verificar()">CADASTRAR-SE</button>
+        <button id="btnSubmit" type="submit" name="submit" class="btn-cadastrar">CADASTRAR-SE</button>
       </form>
     </div>
 
