58565e9e90469bfa62ca8bdf90dfdd048f639641
Murilo Alves da Fonseca <muriloalves.fonseca08@gmail.com>
2025-10-23 21:00:07 -0300

    Acabei criando a pagina de parceiros nessa branch, vou ediar ela em uma outra...

---
+++ html/sejaParceiro.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Seja um Parceiro - Avena</title>
+  <link rel="stylesheet" href="\Programacao_TCC_Avena\css\sejaParceiro.css">
+</head>
+<body>
+
+  <!-- Cabeçalho -->
+  <header>
+    <nav>
+      <div class="logo">
+        <a href="Pagina_Inicial.html">
+          <img src="\Programacao_TCC_Avena\img\logoAvena.png" alt="Logo Avena">
+        </a>
+      </div>
+      <div class="menu">
+        <button class="menu-icon" id="menu-btn">&#9776;</button>
+      </div>  
+    </nav>
+  </header>
+
+  <!-- Menu lateral -->
+  <nav id="menulogin" class="hidden">
+    <ul>
+      <li><a href="sobre.html"><span class="quemSomos">Quem somos</span></a></li>
+      <li><a href="cadastro.php">Cadastrar-se</a></li>
+      <hr>
+      <li><a href="contato.html">Seja um Parceiro</a></li>
+      <li><a href="suporte.html">Suporte</a></li>
+    </ul>
+  </nav>
+
+  <!-- Conteúdo principal -->
+  <main class="parceiro-container">
+  <section class="parceiro-header">
+    <h1>Seja um Parceiro</h1>
+    <p>
+      Na <strong>Avena</strong>, acreditamos que grandes mudanças nascem da colaboração.<br>
+      Buscamos <strong>parceiros que compartilhem da nossa missão</strong> de promover o empoderamento feminino e o fortalecimento do trabalho autônomo.<br>
+      Ao se unir a nós, você contribui para <strong>gerar impacto social, ampliar oportunidades e transformar realidades</strong> — juntos, podemos construir um mercado mais justo e humano.
+    </p>
+
+    <div class="img-row">
+      <div class="circle">
+        <span>ADICIONAR FOTO</span>
+      </div>
+      <div class="circle">
+        <span>ADICIONAR FOTO</span>
+      </div>
+      <div class="circle">
+        <span>ADICIONAR FOTO</span>
+      </div>
+      <div class="circle">
+        <span>ADICIONAR FOTO</span>
+      </div>
+    </div>
+  </section>
+
+  <section class="parceiro-form">
+    <input type="text" placeholder="Digite o seu nome">
+    <input type="email" placeholder="Digite o seu e-mail">
+  </section>
+</main>
+<script src="../js/login.js"></script>
+</body>
+</html>
